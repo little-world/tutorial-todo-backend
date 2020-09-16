4672ede690190fe9b9c9c11fd4b654e0b9f9bbec
peter.van.rijn@little-world.nl <peter.van.rijn@little-world.nl>
2020-09-16 07:45:26 +0200

    second

--- src/main/java/com/example/todo/controllers/TodoRestController.java
+++ src/main/java/com/example/todo/controllers/TodoRestController.java
@@ -3,7 +3,16 @@ package com.example.todo.controllers;
 import com.example.todo.model.Todo;
 import com.example.todo.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -13,8 +22,16 @@ public class TodoRestController {
     TodoService todoService;
 
     @PostMapping("/todo")
-    Todo save(@RequestBody Todo todo) {
+    Todo save(@RequestBody Todo todo) throws Exception {
         return todoService.save(todo);
+    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<FieldError> exceptionHandler(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        return result.getFieldErrors();
     }
 
     @GetMapping("/todo")
@@ -23,7 +40,7 @@ public class TodoRestController {
     }
 
     @DeleteMapping("/todo/{id}")
-    void delete(@PathVariable int id) {
+    void delete(@PathVariable int id){
         todoService.deleteById(id);
     }
 
--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
+
     String task;
 
     public Todo(int id, String task) {
